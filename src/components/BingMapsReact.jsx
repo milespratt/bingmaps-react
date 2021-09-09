2302c9118b18a9d49689b8693720c8763cbeca04
coderfin <coderfin@gmail.com>
2021-09-09 01:29:03 -0600

    return the map instance reference onMapReady

--- src/components/BingMapsReact.jsx
+++ src/components/BingMapsReact.jsx
@@ -138,7 +138,7 @@ export default function BingMapsReact({
         Maps
       );
     }
-    onMapReady && onMapReady();
+    onMapReady && onMapReady({ map });
   }, [
     addPushpinsWithInfoboxes,
     bingMapsKey,
